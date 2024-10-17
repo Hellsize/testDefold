aec2ed2d38ab73598be4302faffa78d499d458dc
Hellsize <dmitrii-walk@mail.ru>
2024-10-17 13:27:37 +0300

    project

---
+++ My Project/assets/gameObjects/spawnPoint.go
@@ -0,0 +1,10 @@
+components {
+  id: "tableController"
+  component: "/assets/scripts/tableController.script"
+}
+embedded_components {
+  id: "animalFactory"
+  type: "factory"
+  data: "prototype: \"/assets/gameObjects/animal.go\"\n"
+  ""
+}
